032aea685a0979285baa70bdbe57ba44c1d9d451
ItayAssouline <Fushionartz@gmail.com>
2019-12-14 17:37:48 +0200

    Added Pacman Class

---
+++ Pacman/Pacman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+	class Pacman
+	{
+		public int x;
+		public int y;
+
+		public int getX()
+		{
+			return this.x;
+		}
+		public int getY()
+		{
+			return this.y;
+		}
+
+		public void ChangeLocation(int xGot, int yGot)
+		{
+			x = xGot;
+			y = yGot;
+
+		}
+
+	}
+}
--- Pacman/Program.cs
+++ Pacman/Program.cs
@@ -16,28 +16,7 @@ namespace Pacman
         Wall,
 
     }
-    class Pacman
-    {
-        public int x;
-        public int y;
-
-        public int getX()
-        {
-            return this.x;
-        }
-        public int getY()
-        {
-            return this.y;
-        }
-
-        public void ChangeLocation(int xGot, int yGot)
-        {
-            x = xGot;
-            y = yGot;
-            
-        }
-       
-    }
+    
 
     class Program
     {
